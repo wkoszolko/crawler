e20e6783247587f5d9181ba49864c9b324d97d14
Wojciech Koszo³ko <wojciech.koszolko@altkominnovation.pl>
2019-06-17 01:26:21 +0200

    Add aspect for logging time execution
    Change-Id: Ia0faee5fc9ee4f32c237f82805a1dc61fe7d5881

--- src/main/java/koszolko/crawler/page/PageFacade.java
+++ src/main/java/koszolko/crawler/page/PageFacade.java
@@ -3,6 +3,7 @@ package koszolko.crawler.page;
 import koszolko.crawler.page.dto.GetPage;
 import koszolko.crawler.page.model.Page;
 import koszolko.crawler.page.service.PageCrawler;
+import koszolko.crawler.shared.logging.LogExecutionTime;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,12 +14,8 @@ import java.util.Optional;
 public class PageFacade {
     private final PageCrawler pageCrawler;
 
+    @LogExecutionTime
     public Optional<Page> fetch(GetPage getPage) {
-        long startTime = System.currentTimeMillis();
-        Optional<Page> page = pageCrawler.crawl(getPage.getUrl());
-        long endTime = System.currentTimeMillis();
-        //todo dodac aspekt logujacy czas wykonania
-        log.info("Fetched url {} in {} ms", getPage.getUrl(), (endTime - startTime));
-        return page;
+        return pageCrawler.crawl(getPage.getUrl());
     }
 }
---
+++ src/main/java/koszolko/crawler/shared/logging/LogExecutionTimeAspect.java
@@ -0,0 +1,24 @@
+package koszolko.crawler.shared.logging;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+@AllArgsConstructor
+class LogExecutionTimeAspect {
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
